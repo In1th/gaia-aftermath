e4046c33fe0e7394ff56337c007edb185aba1dd3
Mateusz Kruk <mateusz64.kruk@student.uj.edu.pl>
2021-08-02 23:30:09 +0200

    Working textbox prototype

--- convo.cpp
+++ convo.cpp
@@ -1,19 +1,95 @@
 #include "include/page.h"
 #include "include/format.h"
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <deque>
 
-int main() {
+Page *exportID(int id);
 
-    Page p;
-    p.symbol = '0';
+int main()
+{
+
+    Page *p = exportID(1);
+
+    /*p.symbol = '0';
     p.name = "Test";
     p.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     p.decision_number = 2;
     p.decisions[0].first = 0;
     p.decisions[0].second = "Nie";
     p.decisions[1].first = 0;
-    p.decisions[1].second = "Tak";
+    p.decisions[1].second = "Tak";*/
 
+    p->display();
 
-    p.display();
+    return 0;
+}
+
+Page *exportID(int id)
+{
+
+    std::ifstream f;
+    Page *output = new Page();
+
+    std::string name = std::to_string(id);
+    f.open("./src/resc/story/" + name + ".dat");
+    if (f.is_open())
+    {
+        std::string line;
+        std::deque<std::string> file;
+        while(std::getline(f,line))
+            file.push_back(line);
+        std::string buf_string;
+        int buf_int;
+        int iterator = 0;
+        std::istringstream iss(file.at(iterator));
+        iterator++;
+        iss >> buf_int;
+        if (id == buf_int) {
+
+            output->ID = id;
+            iss >> buf_string;
+            output->decision_number = std::stoi(buf_string);
+
+            iss >> buf_string;
+
+            if ("1" == buf_string) 
+                output->symbol='0';
+            else {
+                iss >> buf_string;
+                output->symbol=*buf_string.c_str();
+                output->name = file.at(iterator);
+                iterator++;
+            }
+
+            output->text=file.at(iterator);
+            iterator++;
+
+            for (int i = 0; i < output->decision_number; i++){
+                for (int j = 0; j < 4; j++){
+                    std::istringstream iss(file.at(iterator));
+                    iss >> buf_string;
+                    if (buf_string == "!2") {
+                        iss >> buf_int;
+                        iss >> buf_int;
+                        iss >> buf_int;
+                        output->decisions[i].first = buf_int;
+                        iss >> buf_int;
+                    }
+                    iterator++;
+                }
+                output->decisions[i].second = file.at(iterator);
+                iterator++;
+            }
+
+        }
+        else
+            print << "ID podane i w pliku się nie zgadzają!" << eol;
+        f.close();
+    }
+    else
+        print << "Nie ma" << eol;
+
+    return output;
 }
--- src/story_builder.cpp
+++ src/story_builder.cpp
@@ -1,31 +1,146 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
-int main() {
+int main()
+{
 
     std::fstream file;
-    
+    std::ofstream sfile;
+
     int number;
     bool narrator;
-    char frakcja;
+    std::string frakcja;
     std::string nazwa;
     int liczba_opcji;
     std::string tekst;
 
+    file.open("./resc/properties");
 
-    file.open("../src/resc/properties");
+    if (file.is_open())
+    {
+        file >> number;
+        file.close();
 
-    file >> number;
+        number++;
+        std::string nazwa_pliku = std::to_string(number);
 
-    std::cout << "Czy to wypowiedź narratora? (0- nie, 1 - tak): ";
-    std::cin >> narrator;
+        sfile.open("./resc/story/" + nazwa_pliku + ".dat");
+        sfile << number << " ";
 
-    if (!narrator) {
-        std::cout << "Podaj znak frakcji rozmówcy: ";
-        std::cin >> frakcja;
-        std::cout << "Podaj nazwe rozmówcy: ";
-        std::cin >> nazwa;
+        std::cout << "Podaj liczbę wyborów postaci (min 1, max 4): ";
+        std::cin >> liczba_opcji;
+        sfile << liczba_opcji << " ";
+
+        std::cout << "Czy to wypowiedź narratora? (0- nie, 1 - tak): ";
+        std::cin >> narrator;
+        sfile << narrator << " ";
+
+        if (!narrator)
+        {
+            std::cout << "Podaj znak frakcji rozmówcy: ";
+            std::cin.ignore();
+            std::getline(std::cin,frakcja,'\n');
+            std::cout << "Podaj nazwe rozmówcy: ";
+            std::getline(std::cin,nazwa,'\n');
+
+            sfile << frakcja << "\n" << nazwa << "\n";
+        }
+
+        std::cout << "Podaj tekst (bez entera): ";
+        std::getline(std::cin, tekst,'\n');
+
+        sfile << tekst << "\n";
+
+        for (int i = 0; i < liczba_opcji; i++) {
+            int bufor;
+
+            std::cout << std::endl << "OPCJA DIALOGOWA NUMER "<<i+1<<std::endl;
+
+            sfile << "!1 ";
+            std::cout << "Warunek na ukazanie opcji\n";
+            std::cout << "Podaj numer warunku do sprawdzenia (0 - 7) ";
+            std::cin >> bufor;
+            sfile << bufor << " ";
+            if (bufor == 0)
+                sfile << "0\n";
+            else {
+                std::cout << "Podaj wartość warunku która musi być spełniona (0 - 99) ";
+                std::cin >> bufor;
+                if (bufor < 10)
+                    sfile << "0" << bufor << "\n";
+                else
+                    sfile << bufor << "\n";
+
+            }
+
+            sfile << "!2 ";
+            std::cout << "Warunek przy wybraniu opcji\n";
+            std::cout << "Podaj numer warunku do sprawdzenia (0 - 7) ";
+            std::cin >> bufor;
+            sfile << bufor << " ";
+            if (bufor == 0) {
+                sfile << "0 ";
+                std::cout << "Podaj numer skryptu, do którego ma przejść gracz: ";
+                std::cin >> bufor;  
+                if (bufor < 10)
+                    sfile << "0" << bufor << " 00\n";
+                else
+                    sfile << bufor << " 00\n";
+            }
+            else {
+                std::cout << "Podaj wartość warunku która musi być spełniona (0 - 99) ";
+                std::cin >> bufor;
+                if (bufor < 10)
+                    sfile << "0" << bufor << " ";
+                else
+                    sfile << bufor << " ";
+                
+                std::cout << "Podaj numer skryptu, do którego ma przejść gracz przy powodzeniu: ";
+                std::cin >> bufor;
+                sfile << bufor << " ";
+                std::cout << "Podaj numer skryptu, do którego ma przejść gracz przy niepowodzeniu: ";
+                std::cin >> bufor;
+                sfile << bufor << "\n";
+            }
+
+
+                sfile << "!3 ";
+                std::cout << "Czy po ma być dany przedmiot po tym wyborze? (0 - nie, 1 - tak) ";
+                std::cin >> bufor;
+                if (!bufor)
+                    sfile << "0 0 00\n";
+                else {
+                    std::cout << "To jest jeszcze do wdrożenia ~Inith\n";
+                    sfile << "0 0 00\n";
+                }
+
+                sfile << "!4 ";
+                std::cout << "Czy po ma się rozpocząć walka po tym wyborze? (0 - nie, 1 - tak) ";
+                std::cin >> bufor;
+                if (!bufor)
+                    sfile << "0 0 00\n";
+                else {
+                    std::cout << "To jest jeszcze do wdrożenia ~Inith\n";
+                    sfile << "0 0 00\n";
+                }
+
+                std::cout << "Podaj teraz tekst wyboru (bez entera): ";
+                std::cin.ignore();
+                std::getline(std::cin,tekst,'\n');
+                sfile << tekst << "\n";
+                
+            
+        }
+
+        sfile << "@\n"; //do przedmiotów
+        sfile << "#\n"; //do walki
+
+        file.close();
+        sfile.close();
     }
+    else
+        std::cout << "Nie udało się otworzyć pliku" << std::endl;
 
     return 0;
 }
